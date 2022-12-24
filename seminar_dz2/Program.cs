ce9acff69cff81fb6c77d6f5a8a6acf32cdf3244
vjlion <89218741998@mail.ru>
2022-12-24 14:00:32 +0300

    Задача 15

--- seminar_dz2/Program.cs
+++ seminar_dz2/Program.cs
@@ -5,3 +5,18 @@
 7 -> да
 1 -> нет
 */
+Console.Write("Введите цифру: ");
+int day = int.Parse(Console.ReadLine()!);
+
+if (day > 5)
+{
+    Console.Write("Выходной");
+}
+else if (day <= 5)
+{
+   Console.Write("Рабочий");
+}
+else
+{
+   Console.Write("Введите число от 1 до 7");
+}
