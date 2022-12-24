631c5e642acd46cd2ab2c981dfe2c7632066f78e
vjlion <89218741998@mail.ru>
2022-12-24 13:21:25 +0300

    Задача 10

---
+++ seminar_dz1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+int GetNumberFromRange(int start, int end)
+{
+    int number = new Random().Next(start, end + 1);
+    return number;
+}
+int Delnum(int number)
+{
+    int s = (number % 100) / 10;
+    return s;
+}
+int randomNunber = GetNumberFromRange(100,999);
+int numberWithoutSecond = Delnum(randomNunber) ;
+Console.WriteLine($"Число {randomNunber} вторая цифра этого числа {numberWithoutSecond}");
